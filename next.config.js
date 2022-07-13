5e0d739233bf705f45390c91f0751db263446b77
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-07-13 12:49:10 +0200

    Fix custom MDX components
    Co-authored-by: Joel Plumppu <paccao@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 const nextMDX = require('@next/mdx')
 
 const withMDX = nextMDX({
-    // By default only the .mdx extension is supported.
     extension: /\.mdx?$/,
+    options: {
+        providerImportSource: '@mdx-js/react',
+    },
 })
 
 module.exports = withMDX({
-    // Support MDX files as pages:
     pageExtensions: ['mdx', 'tsx', 'ts', 'jsx', 'js'],
     reactStrictMode: true,
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback.fs = false
-        }
-        return config
-    },
 })
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import { FC } from 'react'
+import { FC, forwardRef } from 'react'
 import type { AppProps } from 'next/app'
 import { MDXProvider } from '@mdx-js/react'
 import { MDXComponents } from 'mdx/types'
 import Image, { ImageProps } from 'next/image'
+import Link, { LinkProps } from 'next/link'
 
 import '../styles/globals.css'
 
@@ -10,9 +11,25 @@ const ResponsiveImage: FC<ImageProps> = (props) => (
     <Image {...props} layout="responsive" alt={props.alt} />
 )
 
+const CustomLink: typeof Link = forwardRef(function CustomLink(
+    { children, ...props },
+    ref,
+) {
+    console.log('link', props, children)
+    return (
+        <Link {...props}>
+            <a className="underline text-black bg-red-500" ref={ref}>
+                {children}
+            </a>
+        </Link>
+    )
+})
+
 const components = {
     image: ResponsiveImage,
-} as MDXComponents
+    a: CustomLink,
+    p: (props: any) => <p {...props} className="bg-red-200" />,
+} as unknown as MDXComponents
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
