97ee484376e7889c60c0eec8aefe043f28197c89
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-07-17 21:39:54 +0200

    move header out of main tag

--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -10,20 +10,19 @@ export type LandingLayoutProps = {
 
 const LandingLayout: FC<LandingLayoutProps> = ({ children }) => {
     return (
-        <main>
+        <>
             <Head>
                 <title>{SITE_NAME}</title>
                 <meta name="description" content={SITE_DESCRIPTION} />
             </Head>
-
             <Header />
-
-            <article className='lg:prose-xl"> prose prose-stone mx-auto marker:text-stone-900 prose-p:leading-6 prose-p:text-stone-900 prose-strong:text-stone-900 prose-em:text-stone-900 prose-li:my-1 prose-li:text-stone-900 md:prose-lg'>
-                {children}
-            </article>
-        </main>
+            <main>
+                <article className='lg:prose-xl"> prose prose-stone mx-auto marker:text-stone-900 prose-p:leading-6 prose-p:text-stone-900 prose-strong:text-stone-900 prose-em:text-stone-900 prose-li:my-1 prose-li:text-stone-900 md:prose-lg'>
+                    {children}
+                </article>
+            </main>
+        </>
     )
 }
 
 export default LandingLayout
-
--- src/layouts/PostLayout.tsx
+++ src/layouts/PostLayout.tsx
@@ -10,21 +10,20 @@ export type PostLayoutProps = {
 
 const PostLayout: FC<PostLayoutProps> = ({ children }) => {
     return (
-        <main>
+        <>
             {/* IDEA: Use specific meta tags based on post content: title, description, image, timestamp/date */}
             <Head>
                 <title>{SITE_NAME}</title>
                 <meta name="description" content={SITE_DESCRIPTION} />
             </Head>
-
             <Header />
-
-            <article className="prose prose-stone mx-auto marker:text-stone-900 prose-p:leading-6 prose-p:text-stone-900 prose-strong:text-stone-900 prose-em:text-stone-900 prose-li:my-1 prose-li:text-stone-900 md:prose-lg lg:prose-xl">
-                {children}
-            </article>
-        </main>
+            <main>
+                <article className="prose prose-stone mx-auto marker:text-stone-900 prose-p:leading-6 prose-p:text-stone-900 prose-strong:text-stone-900 prose-em:text-stone-900 prose-li:my-1 prose-li:text-stone-900 md:prose-lg lg:prose-xl">
+                    {children}
+                </article>
+            </main>
+        </>
     )
 }
 
 export default PostLayout
-
