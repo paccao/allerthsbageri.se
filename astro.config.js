6d2c5419285ecfa1105cb04a1fef654e07d70362
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2023-04-26 22:12:21 +0200

    overwrite next with astro, copy git history
    Co-authored-by: Samuel Plumppu <Greenheart@users.noreply.github.com>

---
+++ astro.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://allerthsbageri.se',
+	integrations: [tailwind()],
+	experimental: {
+		assets: true,
+	},
+	image: {
+		service: 'astro/assets/services/sharp',
+	},
+})
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+    content: ['./src/**/*.{astro,html,js,ts,md,mdx}'],
     screens: {
+        '2xs': '375px',
         xs: '475px',
         ...defaultTheme.screens,
     },
