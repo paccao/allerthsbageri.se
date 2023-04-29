7398e05f742f58bf45a2f2542f6ef2e3883e329d
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2023-04-29 14:41:18 +0200

    add display separate at medium screen width

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/**/*.{astro,html,js,ts,md,mdx}'],
   screens: {
+    '3xs': '300px',
     '2xs': '375px',
     xs: '475px',
     ...defaultTheme.screens,
