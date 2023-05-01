9ea12b3c811c7e4f960f6123bb6df0eb3a04c46c
paccao <16489683+paccao@users.noreply.github.com>
2023-05-01 13:51:15 +0200

    add more screen sizes to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,18 +3,17 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,ts,md,mdx}'],
-  screens: {
-    '3xs': '300px',
-    '2xs': '375px',
-    xs: '475px',
-    ...defaultTheme.screens,
-  },
   theme: {
     extend: {
       fontFamily: {
         display: ['Italianno', 'cursive'],
         serif: ['Quattrocento', 'serif'],
       },
+    },
+    screens: {
+      '3xs': '300px',
+      '2xs': '375px',
+      xs: '475px',
     },
   },
   plugins: [require('@tailwindcss/typography')],
