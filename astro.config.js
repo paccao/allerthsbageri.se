bac217391ea29325ab72fdafa1fa5f25c62481cc
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-10-19 15:12:05 +0200

    Remove vite plugin for basic SSL, since that is not needed on localhost

--- astro.config.js
+++ astro.config.js
@@ -2,7 +2,6 @@ import { defineConfig, passthroughImageService } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import svelte from '@astrojs/svelte'
-import basicSSL from '@vitejs/plugin-basic-ssl'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,8 +9,5 @@ export default defineConfig({
   integrations: [tailwind(), svelte(), sitemap()],
   image: {
     service: passthroughImageService(),
-  },
-  vite: {
-    plugins: [basicSSL()],
   },
 })
