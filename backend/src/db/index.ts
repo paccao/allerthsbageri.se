8dad5577b690f741e277e299e5f255e845cdbec1
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-12-25 12:58:20 +0100

    Use consistent casing

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,4 +2,7 @@ import { drizzle } from 'drizzle-orm/better-sqlite3'
 
 import apiConfig from '../config/api'
 
-export const db = drizzle(apiConfig.dbConnection)
+export const db = drizzle({
+  connection: apiConfig.dbConnection,
+  casing: 'snake_case',
+})
