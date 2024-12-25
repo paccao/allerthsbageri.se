8e20ac365f07d5c1518630b7d5bcf3b879567d6c
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-12-25 13:07:51 +0100

    Add initial DB migration

---
+++ backend/migrations/0000_init.sql
@@ -0,0 +1,66 @@
+CREATE TABLE `customer` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text(200) NOT NULL,
+	`phone` text(50) NOT NULL
+);
+
+CREATE TABLE `pickup_order` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`booking_opens` text(30) NOT NULL,
+	`customer_id` integer NOT NULL,
+	`pickup_occasion_id` integer NOT NULL,
+	`status_id` integer NOT NULL,
+	FOREIGN KEY (`customer_id`) REFERENCES `customer`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`pickup_occasion_id`) REFERENCES `pickup_occasion`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`status_id`) REFERENCES `order_status`(`id`) ON UPDATE no action ON DELETE no action
+);
+
+CREATE TABLE `order_item` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`count` integer NOT NULL,
+	`price` integer NOT NULL,
+	`product_id` integer NOT NULL,
+	`order_id` integer NOT NULL,
+	FOREIGN KEY (`product_id`) REFERENCES `product`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`order_id`) REFERENCES `pickup_order`(`id`) ON UPDATE no action ON DELETE no action
+);
+
+CREATE TABLE `order_status` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`status` text(50) NOT NULL,
+	`color` text(50)
+);
+
+CREATE TABLE `pickup_occasion` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text(200) NOT NULL,
+	`description` text(1000) NOT NULL,
+	`booking_opens` text(30) NOT NULL
+);
+
+CREATE TABLE `product` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`stock` integer NOT NULL,
+	`price` integer NOT NULL,
+	`pickup_occasion_id` integer NOT NULL,
+	`product_details_id` integer NOT NULL,
+	FOREIGN KEY (`pickup_occasion_id`) REFERENCES `pickup_occasion`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`product_details_id`) REFERENCES `product_details`(`id`) ON UPDATE no action ON DELETE no action
+);
+
+CREATE TABLE `product_details` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text(200) NOT NULL,
+	`description` text(1000) NOT NULL,
+	`image` text,
+	`vat_percentage` integer NOT NULL
+);
+
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text(200) NOT NULL,
+	`username` text(30) NOT NULL,
+	`password` text(100) NOT NULL
+);
+
+CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);
