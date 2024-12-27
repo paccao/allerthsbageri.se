ac11b0033fd4103aa5056382c66d809b9b7bd88f
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-12-28 00:26:34 +0100

    Enforce stricter phone number validation by using libphonenumber-js

--- backend/src/modules/customer/customer.schemas.ts
+++ backend/src/modules/customer/customer.schemas.ts
@@ -1,10 +1,9 @@
+import { zPhone } from '@/utils/zod.ts'
 import { z } from 'zod'
 
 export const createCustomerBodySchema = z.object({
   name: z.string().max(200),
-  // TODO: Enforce stricter phone number validation
-  // by using libphonenumber-js
-  phone: z.string().max(50),
+  phone: zPhone,
 })
 
 export type CreateCustomerBody = z.infer<typeof createCustomerBodySchema>
