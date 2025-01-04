ebac3b357adb7d5f143a663a30ce52c31719ae35
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-01-04 23:18:07 +0100

    Organize as test suite and allow running specific tests

--- backend/src/app.test.ts
+++ backend/src/app.test.ts
@@ -1,26 +1,26 @@
-import { test, type TestContext } from 'node:test'
+import { suite, test, type TestContext } from 'node:test'
 
-import startApp from './app.ts'
+import startApp from '#src/app.ts'
 
 const app = await startApp()
 
-test('GET /health-check should return status OK', async (t: TestContext) => {
-  const response = await app.inject({
-    method: 'GET',
-    url: '/health-check',
+suite('app setup', () => {
+  test('GET /health-check should return status OK', async (t: TestContext) => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/health-check',
+    })
+
+    t.assert.strictEqual(response.statusCode, 200)
+    t.assert.deepStrictEqual(response.json(), { ok: true })
   })
 
-  t.assert.strictEqual(response.statusCode, 200)
-  t.assert.deepStrictEqual(response.json(), { ok: true })
-})
+  test('OpenAPI docs should only be available during development', async (t: TestContext) => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/api/docs',
+    })
 
-test('OpenAPI docs should only be available during development', async (t: TestContext) => {
-  const response = await app.inject({
-    method: 'GET',
-    url: '/api/docs',
+    t.assert.strictEqual(response.statusCode, 404)
   })
-
-  t.assert.strictEqual(response.statusCode, 404)
 })
-
-// Check out more about testing at https://fastify.dev/docs/latest/Guides/Testing/
