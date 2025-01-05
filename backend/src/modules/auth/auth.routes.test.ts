5c0fb42ffbef2cf3e806a38fcc0b81800beef81e
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-01-05 16:57:38 +0100

    Test sign out and improve tests for auth process

--- backend/src/modules/auth/auth.routes.test.ts
+++ backend/src/modules/auth/auth.routes.test.ts
@@ -5,6 +5,10 @@ import apiConfig from '#config/api.ts'
 
 const app = await startApp()
 
+function getSessionCookie(response: Awaited<ReturnType<typeof app.inject>>) {
+  return response.cookies.find((c) => c.name === apiConfig.sessionCookieName)
+}
+
 suite.only('auth routes', () => {
   test.only('should be possible to sign up', async (t: TestContext) => {
     const response = await app.inject({
@@ -17,11 +21,9 @@ suite.only('auth routes', () => {
       },
     })
 
-    const hasSessionCookie = response.cookies.some(
-      (c) => c.name === apiConfig.sessionCookieName,
-    )
+    const sessionCookie = getSessionCookie(response)
 
-    t.assert.strictEqual(hasSessionCookie, true)
+    t.assert.notStrictEqual(sessionCookie, undefined)
   })
 
   test.only('should be possible to sign in', async (t: TestContext) => {
@@ -44,10 +46,48 @@ suite.only('auth routes', () => {
       },
     })
 
-    const hasSessionCookie = signInResponse.cookies.some(
-      (c) => c.name === apiConfig.sessionCookieName,
-    )
+    const sessionCookie = getSessionCookie(signInResponse)
 
-    t.assert.strictEqual(hasSessionCookie, true)
+    t.assert.notStrictEqual(sessionCookie, undefined)
+  })
+
+  test.only('should be possible to sign out', async (t: TestContext) => {
+    await app.inject({
+      method: 'POST',
+      url: '/api/auth/sign-up',
+      body: {
+        username: 'admin3',
+        name: 'Admin3',
+        password: '123456',
+      },
+    })
+
+    const signInResponse = await app.inject({
+      method: 'POST',
+      url: '/api/auth/sign-in',
+      body: {
+        username: 'admin3',
+        password: '123456',
+      },
+    })
+
+    const sessionCookie = getSessionCookie(signInResponse)
+
+    t.assert.notStrictEqual(sessionCookie, undefined)
+    t.assert.notStrictEqual(sessionCookie?.expires, undefined)
+    t.assert.strictEqual(sessionCookie?.maxAge, undefined)
+
+    const signOutResponse = await app.inject({
+      method: 'POST',
+      url: '/api/auth/sign-out',
+      headers: {
+        cookie: signInResponse.headers['set-cookie'],
+      },
+    })
+
+    const signOutSessionCookie = getSessionCookie(signOutResponse)
+
+    t.assert.notStrictEqual(signOutSessionCookie, undefined)
+    t.assert.strictEqual(signOutSessionCookie?.maxAge, 0)
   })
 })
