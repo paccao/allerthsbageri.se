17d1cbf74221329074d9c471ff30d08267c4c7a6
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-07-25 23:05:38 +0200

    Fix zod type error due to migrationto v4

--- backend/src/modules/pickup/pickup.schemas.ts
+++ backend/src/modules/pickup/pickup.schemas.ts
@@ -13,14 +13,14 @@ export const createPickupBodySchema = z
     ({ bookingOpens, bookingCloses, pickupOpens, pickupCloses }, ctx) => {
       if (bookingOpens.getTime() > bookingCloses.getTime()) {
         ctx.addIssue({
-          code: z.ZodIssueCode.invalid_date,
+          code: 'custom',
           message: 'bookingCloses must be after bookingOpens',
         })
       }
 
       if (pickupOpens.getTime() > pickupCloses.getTime()) {
         ctx.addIssue({
-          code: z.ZodIssueCode.invalid_date,
+          code: 'custom',
           message: 'pickupCloses must be after pickupOpens',
         })
       }
