701c58d6751e32f19027e56c815fd56fdc61ed96
paccao <16489683+paccao@users.noreply.github.com>
2025-07-26 19:53:32 +0200

    Add chadcn svelte dep

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,11 +1,25 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
+
 /**
  * Call a function once and then remove it.
  * This is useful for event handlers that should only run once.
  */
 export function once(fn: Function) {
-  return function (this: Function, event: Event) {
-    if (fn) fn.call(this, event)
-    // @ts-expect-error
-    fn = null
-  }
+	return function (this: Function, event: Event) {
+		if (fn) fn.call(this, event)
+		// @ts-expect-error
+		fn = null
+	}
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
+export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
