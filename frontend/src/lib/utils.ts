5c6aee502ab932a1526cf740d95bc068d1743433
paccao <16489683+paccao@users.noreply.github.com>
2025-07-26 19:57:28 +0200

    format files

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,8 +1,8 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 /**
@@ -10,16 +10,20 @@ export function cn(...inputs: ClassValue[]) {
  * This is useful for event handlers that should only run once.
  */
 export function once(fn: Function) {
-	return function (this: Function, event: Event) {
-		if (fn) fn.call(this, event)
-		// @ts-expect-error
-		fn = null
-	}
+  return function (this: Function, event: Event) {
+    if (fn) fn.call(this, event)
+    // @ts-expect-error
+    fn = null
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
+export type WithoutChild<T> = T extends { child?: any } ? Omit<T, 'child'> : T
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
-export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
-export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
+export type WithoutChildren<T> = T extends { children?: any }
+  ? Omit<T, 'children'>
+  : T
+export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {
+  ref?: U | null
+}
