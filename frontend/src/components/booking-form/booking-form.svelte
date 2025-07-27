bf5ab22bb9882c842aca6aa67a57a72ce2c586b0
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-07-27 19:00:45 +0200

    Add mock data for the order form

---
+++ frontend/src/components/booking-form/booking-form.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  // TODO: state management for the booking process: pickup occasion, products and amounts, contact details
+  // TODO: steps as separate snippets that get rendered by selecting the current step
+
+  const pickupOccasions = [
+    {
+      id: 1,
+      location: 'REKO-ringen Borås',
+      startTime: new Date('2025-08-23T10:00:00.000Z'),
+      endTime: new Date('2025-08-23T15:30:00.000Z'),
+      products: [
+        {
+          id: 1,
+          stock: 50,
+          price: 6900n,
+          pickupOccasionId: 1,
+          name: 'Ekologiskt Surdegsbröd',
+          description:
+            'Bakat med färskmalen ekologisk emmer som är ett kultursädesslag som började odlas för 8 000 år sedan och är en korsning mellan enkorn och vildgräs.',
+        },
+        {
+          id: 2,
+          stock: 25,
+          price: 6500n,
+          pickupOccasionId: 1,
+          name: 'Ekologiskt Rågsurdegsbröd',
+          description: 'Bakat med färskmalet ekologiskt fullkornsrågmjöl.',
+        },
+      ],
+    },
+    {
+      id: 2,
+      location: 'REKO-ringen Ulricehamn',
+      startTime: new Date('2025-08-23T10:00:00.000Z'),
+      endTime: new Date('2025-08-23T15:30:00.000Z'),
+      products: [
+        {
+          id: 1,
+          stock: 50,
+          price: 6900n,
+          pickupOccasionId: 2,
+          name: 'Ekologiskt Surdegsbröd',
+          description:
+            'Bakat med färskmalen ekologisk emmer som är ett kultursädesslag som började odlas för 8 000 år sedan och är en korsning mellan enkorn och vildgräs.',
+        },
+        {
+          id: 2,
+          stock: 25,
+          price: 6500n,
+          pickupOccasionId: 2,
+          name: 'Ekologiskt Rågsurdegsbröd',
+          description: 'Bakat med färskmalet ekologiskt fullkornsrågmjöl.',
+        },
+      ],
+    },
+  ]
+
+  type PickupOccasion = (typeof pickupOccasions)[number]
+  type Product = (typeof pickupOccasions)[number]['products'][number]
+</script>
