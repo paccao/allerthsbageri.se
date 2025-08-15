8e4faa919cf3bb3bf1158955875329d7c93dda15
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-08-15 20:52:33 +0200

    Improve card layout for products

--- frontend/src/components/booking-form/products.svelte
+++ frontend/src/components/booking-form/products.svelte
@@ -1,23 +1,32 @@
 <script lang="ts">
+  import { Button } from '$components/ui/button'
+  import * as Card from '$components/ui/card'
   import { toSEKString } from '$lib/currency'
   import { bookingContext } from './context'
   const ctx = bookingContext.get()
 </script>
 
-<!-- Grid with up to 4 columns on desktop and down to 1 on mobile -->
 <!-- Each product can be selected with a + button that then transforms into a button row with an number input in the middle -->
 
 {#if ctx.pickupOccasion}
-  <div class="grid gap-4">
-    {#each ctx.pickupOccasion.products as { name, description, price }}
-      <!-- TODO: Use card component here -->
-      <div class="p-4">
-        <h2 class="font-bold">{name}</h2>
-        <p>{description}</p>
-        <p class="font-semibold">
-          {toSEKString(price)}
-        </p>
-      </div>
+  <div class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(256px,1fr))]">
+    {#each [ctx.pickupOccasion.products, ctx.pickupOccasion.products].flat() as { name, description, price }}
+      <Card.Root class="gap-4">
+        <Card.Header>
+          <Card.Title class="font-bold text-lg">{name}</Card.Title>
+        </Card.Header>
+        <Card.Content class="grow">
+          <Card.Description class="text-black/85"
+            >{description}</Card.Description
+          >
+          <p class="font-black pt-2">
+            {toSEKString(price)}
+          </p>
+        </Card.Content>
+        <Card.Footer>
+          <Button class="w-full self-end" size="xl">Lägg i varukorg</Button>
+        </Card.Footer>
+      </Card.Root>
     {/each}
   </div>
 {/if}
