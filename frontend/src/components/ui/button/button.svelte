f60297f412e5a7c43d65c98ea97c165212a03b3b
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-08-15 11:45:20 +0200

    Add xl button size

--- frontend/src/components/ui/button/button.svelte
+++ frontend/src/components/ui/button/button.svelte
@@ -26,6 +26,7 @@
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
         sm: 'h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
+        xl: 'h-12 rounded-md px-6 has-[>svg]:px-4 text-base',
         icon: 'size-9',
       },
     },
