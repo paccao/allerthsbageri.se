c6eddf8940b2bc21e0c084909388b883e05c4558
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-10-02 13:46:26 +0200

    List order items and show total price

--- frontend/src/components/booking-form/order.svelte
+++ frontend/src/components/booking-form/order.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
   import * as Card from '$components/ui/card'
   import { bookingContext } from './context'
+  import { toSEKString } from '$lib/currency'
+  import type { Product } from './booking-form.svelte'
 
   const ctx = bookingContext.get()
+
+  const orderItems = $derived(
+    Object.entries(ctx.order.items).reduce<(Product & { count: number })[]>(
+      (orderItems, [productId, count]) => {
+        const id = Number(productId)
+        if (ctx.order.items[id]) {
+          orderItems.push({
+            ...ctx.pickupOccasion!.products.find((p) => p.id === id)!,
+            count,
+          })
+        }
+        return orderItems
+      },
+      [],
+    ),
+  )
+
+  const totalPrice = $derived(
+    orderItems.reduce((total, { price }) => total + price, 0n),
+  )
 </script>
 
 <div class="grid gap-x-4 gap-y-8 sm:grid-cols-[3fr_2fr]">
@@ -15,15 +37,27 @@
     <Card.Header>
       <Card.Title class="font-bold text-lg">Varukorg</Card.Title>
     </Card.Header>
-    <Card.Content class="grow">
-      <!-- TODO: Product item name -->
-      <!-- Show the count for each product -->
-      <!-- TODO: Show the buttons to add/remove products -->
-      <!-- TODO: If a product is completely removed from the order, remove it from the list -->
-      <!-- TODO: Show the total price -->
-      <!-- {#each ctx.order.items as item}
-        {item}
-      {/each} -->
-    </Card.Content>
+
+    {#if ctx.pickupOccasion && ctx.order}
+      <Card.Content class="grow">
+        <div class="grid gap-1 font-bold">
+          {#each orderItems as { id, name, count } (id)}
+            <div class="flex justify-between">
+              <h2 class="font-bold">{name}</h2>
+              <span>{count}</span>
+            </div>
+            <!-- TODO: Show the buttons to add/remove products -->
+            <!-- TODO: If a product is completely removed from the order, remove it from the list. This should happen automatically when the state is updated -->
+          {/each}
+        </div>
+
+        <hr class="my-4" />
+
+        <p class="font-bold flex justify-between">
+          <span>Att betala</span>
+          <span>{toSEKString(totalPrice)}</span>
+        </p>
+      </Card.Content>
+    {/if}
   </Card.Root>
 </div>
