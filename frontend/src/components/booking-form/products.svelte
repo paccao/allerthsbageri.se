f4c4458d0d6b95a46470d6b6619e26a5934c65da
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-10-07 22:29:24 +0200

    Begin new pickup occasion selection

--- frontend/src/components/booking-form/products.svelte
+++ frontend/src/components/booking-form/products.svelte
@@ -4,8 +4,18 @@
   import { toSEKString } from '$lib/currency'
   import { bookingContext } from './context'
   import ProductCount from './product-count.svelte'
+  import LucideCalendarClock from '~icons/lucide/calendar-clock'
+  import LucideCheck from '~icons/lucide/check'
 
   const ctx = bookingContext.get()
+
+  const dateTimeFormatter = new Intl.DateTimeFormat('sv-SE', {
+    day: 'numeric',
+    month: 'short',
+    weekday: 'short',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
 </script>
 
 <!--
@@ -25,6 +35,40 @@ By showing products earlier, it will be more inviting to complete orders.
 -->
 <!-- IDEA: Alternatively, show the pickup occasion on the products page. Maybe thin banner at the top -->
 <!-- TODO: Only show the two upcoming pickup occasions -->
+
+{#each ctx.pickupOccasions as pickup}
+  {@const dateTime = dateTimeFormatter.formatRange(
+    pickup.startTime,
+    pickup.endTime,
+  )}
+  {@const isSelected = ctx.order.pickupOccasionId === pickup.id}
+  <button
+    onclick={() => ctx.selectPickupOccasion(pickup)}
+    aria-label="Välj upphämtningstillfälle {dateTime}"
+    class="group cursor-pointer max-w-(--breakpoint-lg) mx-auto w-full p-4"
+  >
+    <div
+      class={[
+        'p-4 flex justify-between items-center border border-black rounded-lg hover:border-primary hover:bg-black/5 group-focus-within:border-primary group-focus-within:bg-black/5',
+        isSelected && 'border-primary bg-black/5',
+      ]}
+    >
+      <div class="flex flex-col justify-between items-start">
+        <h2 class="font-bold flex items-center gap-2">
+          <LucideCalendarClock class="size-4 inline" />
+          <span class="first-letter:capitalize">{dateTime}</span>
+        </h2>
+        <p>{pickup.location}</p>
+      </div>
+
+      {#if isSelected}
+        <LucideCheck
+          class="size-7 sm:size-8 bg-black text-white rounded-full p-2 shadow-xl"
+        />
+      {/if}
+    </div>
+  </button>
+{/each}
 
 {#if ctx.pickupOccasion}
   <div
