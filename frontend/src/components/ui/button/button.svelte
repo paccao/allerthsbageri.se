3c2df455dc58640005415a318885001d656c909d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-10-09 12:50:32 +0200

    Update button component

--- frontend/src/components/ui/button/button.svelte
+++ frontend/src/components/ui/button/button.svelte
@@ -7,7 +7,7 @@
   import { type VariantProps, tv } from 'tailwind-variants'
 
   export const buttonVariants = tv({
-    base: "select-none cursor-pointer focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium outline-none transition-colors focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+    base: "select-none focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium outline-none transition-colors focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
     variants: {
       variant: {
         default:
@@ -28,6 +28,8 @@
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
         xl: 'h-12 rounded-md px-6 has-[>svg]:px-4 text-base',
         icon: 'size-9',
+        'icon-sm': 'size-8',
+        'icon-lg': 'size-10',
       },
     },
     defaultVariants: {
