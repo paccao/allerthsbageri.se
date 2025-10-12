a0817f1dd1a710d0f882bae23f5889de44b63daf
paccao <16489683+paccao@users.noreply.github.com>
2025-10-12 17:06:41 +0200

    add patch product details integration test

--- backend/src/modules/product-details/product-details.routes.test.ts
+++ backend/src/modules/product-details/product-details.routes.test.ts
@@ -72,6 +72,90 @@ suite('product details routes', () => {
     )
   })
 
+  test('should only patch data that was sent', async (t: TestContext) => {
+    const productDetail = {
+      name: 'blåbärmuffin',
+      description: 'Gjord på blåbär, surdeg och kärlek',
+      image: null,
+      vatPercentage: 40,
+    }
+
+    const createResponse = await app.inject({
+      method: 'POST',
+      url: '/api/product-details/',
+      body: productDetail,
+      headers: { cookie },
+    })
+
+    const createDeserialized = createResponse.json()
+
+    t.assert.strictEqual(createResponse.statusCode, 201)
+
+    const productDetail2 = {
+      name: 'hallonmuffin',
+      description: 'Gjord på hallon, surdeg och kärlek',
+      image: 'hej',
+      vatPercentage: 15,
+    }
+
+    const anotherCreatedProductDetail = await app
+      .inject({
+        method: 'POST',
+        url: '/api/product-details/',
+        body: productDetail2,
+        headers: { cookie },
+      })
+      .then((res) => res.json())
+
+    const patchDetails = {
+      name: 'blåbärmuffin93428498',
+      description: 'Gjord på blåbär, surdeg och armbåge',
+      image: 'https://www.allerthsbageri.se/content/bild123',
+      vatPercentage: 12,
+    }
+
+    const afterUpdate = await app.inject({
+      method: 'PATCH',
+      url: `/api/product-details/${createDeserialized.id}`,
+      body: patchDetails,
+      headers: { cookie },
+    })
+
+    const patchedDeserialized = afterUpdate.json()
+
+    const getProductDetail2 = await app
+      .inject({
+        method: 'GET',
+        url: `/api/product-details/${anotherCreatedProductDetail.id}`,
+        headers: { cookie },
+      })
+      .then((res) => res.json())
+
+    t.assert.strictEqual(anotherCreatedProductDetail.id, getProductDetail2.id)
+
+    t.assert.strictEqual(afterUpdate.statusCode, 200)
+    t.assert.strictEqual(
+      createDeserialized.id,
+      patchedDeserialized.id,
+      'assert that id has not changed',
+    )
+    t.assert.notStrictEqual(
+      {
+        name: createDeserialized.name,
+        description: createDeserialized.description,
+        image: createDeserialized.image,
+        vatPercentage: createDeserialized.vatPercentage,
+      },
+      {
+        name: patchedDeserialized.name,
+        description: patchedDeserialized.description,
+        image: patchedDeserialized.image,
+        vatPercentage: patchedDeserialized.vatPercentage,
+      },
+      'assert that the data changed',
+    )
+  })
+
   after(async () => {
     await db
       .delete(userTable)
