5efb779131db669733f9131970d6f31a6568b2aa
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-10-13 11:01:46 +0200

    Setup project dependencies

--- sk-frontend/svelte.config.ts
+++ sk-frontend/svelte.config.ts
@@ -1,15 +1,20 @@
 import adapter from '@sveltejs/adapter-node'
+import { type Config } from '@sveltejs/kit'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config: Config = {
   preprocess: vitePreprocess(),
+
+  // TODO: Add existing ui components
+  // TODO: make it work
 
   kit: {
     adapter: adapter(),
     alias: {
       $components: './src/components',
       '$components/*': './src/components/*',
+      $assets: './src/assets',
+      '$assets/*': './src/assets/*',
     },
   },
 }
