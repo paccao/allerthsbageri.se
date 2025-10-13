0c51b57f57e195c53fcf8fea5d9ddd55680f35b9
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-10-13 17:09:06 +0200

    Add layout

---
+++ sk-frontend/src/lib/components/layouts/page-layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Navbar from '$components/navbar.svelte'
+  import Footer from '$components/footer.svelte'
+  import { cn } from '$lib/utils'
+  import type { Snippet } from 'svelte'
+
+  type Props = {
+    class?: string
+    header?: boolean
+    footer?: boolean
+    children?: Snippet
+  }
+
+  let {
+    class: className = '',
+    header = true,
+    footer = true,
+    children,
+  }: Props = $props()
+</script>
+
+{#if header}
+  <Navbar />
+{/if}
+
+<main class={cn('mx-auto w-full', className)}>
+  {@render children?.()}
+</main>
+
+{#if footer}
+  <Footer />
+{/if}
---
+++ sk-frontend/src/lib/components/navbar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import allerthsBageriLogo from '$assets/allerths-bageri-logo.jpeg'
+  import { SITE_NAME } from '$lib/constants'
+  import { cn } from '$lib/utils'
+  import { buttonVariants } from '$components/ui/button'
+  import { page } from '$app/state'
+
+  page.url.pathname
+</script>
+
+<nav class="flex items-center justify-between px-4 py-2 shadow-sm">
+  <a class="flex items-center gap-4" href="/">
+    <enhanced:img
+      src={allerthsBageriLogo}
+      alt="Profilbild för Allerths Bageri"
+      class="size-12 rounded-full object-cover shadow-md"
+      loading="eager"
+      fetchpriority="high"
+    />
+    <h1 class="font-display xs:text-4xl pt-2 text-3xl font-bold">
+      {SITE_NAME}
+    </h1>
+  </a>
+
+  <ul class="flex items-center gap-4">
+    {#if page.url.pathname !== '/bokning/'}
+      <li>
+        <a
+          href="/bokning"
+          class={cn([buttonVariants({ variant: 'outline', size: 'lg' })])}
+        >
+          Boka ditt bröd!
+        </a>
+      </li>
+    {/if}
+  </ul>
+</nav>
