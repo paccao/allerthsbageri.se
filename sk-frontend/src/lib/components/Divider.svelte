8664d530b122a4bb9d3ae9f4811742cbf53b1f3d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-11-05 19:17:05 +0100

    Add start page

---
+++ sk-frontend/src/lib/components/Divider.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { cn } from '$lib/utils'
+    
+    type Props = {
+      class?: string
+    }
+    let { class: className }: Props = $props()
+</script>
+
+
+<span class={cn('my-12 w-7 mx-auto border-t flex', className)}></span>
