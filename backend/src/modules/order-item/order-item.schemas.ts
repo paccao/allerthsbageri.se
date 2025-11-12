ccb0c2f3e8f18a946f968b0c0de8408557546def
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-11-12 10:59:39 +0100

    Clarify schema
    Co-authored-by: Joel Plumppu <paccao@users.noreply.github.com>

--- backend/src/modules/order-item/order-item.schemas.ts
+++ backend/src/modules/order-item/order-item.schemas.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod'
 
 export const createOrderItemBodySchema = z.object({
-  count: z.int().min(0), // todo: max value validated in the handler
+  /** The max value should be validated in the handler */
+  count: z.int().min(0),
   productId: z.int().min(1),
 })
 export type CreateOrderItemSchema = z.infer<typeof createOrderItemBodySchema>
