c7c7743223798f3f0ff6c84916dd1c124bc39a96
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-11-28 21:55:03 +0100

    WIP: Generate a migration, apply it and add seeding data

--- backend/src/db/test-db.ts
+++ backend/src/db/test-db.ts
@@ -4,41 +4,50 @@ import { pushSQLiteSchema } from 'drizzle-kit/api'
 
 import apiConfig from '#config/api.ts'
 import * as schema from '#db/schema.ts'
+import { addSeedingData } from './seed.ts'
 
-if (!apiConfig.env.TEST) {
-  throw new Error('This module should only be used for tests')
-}
+// TODO: Re-enable when done
+// if (!apiConfig.env.TEST) {
+//   throw new Error('This module should only be used for tests')
+// }
 
 export async function setupMockedInMemoryTestDB() {
-  // TODO: Is it possible to mock the #db/index.ts module directly in here?
-  // since it is imported by the same process as the node:test suite
-  // OR do we need to create the mock in the suite itself?
-
   const db = drizzle({
-    connection: { source: apiConfig.dbConnection },
+    // TODO: Switch back when done
+    // connection: { source: apiConfig.dbConnection },
+    connection: { source: ':memory:' },
     casing: 'snake_case',
     schema,
   })
 
-  // TODO: Is there a way to push directly to the in-memory DB?
-  // Maybe we could use pushSQLiteSchema()?
-  // TODO: Search in the Drizzle codebase for pushSQLiteSchema() usage examples
-  // Maybe we could use it to easily prepare the in-memory DB
+  // Use the latest DB schema to generate a SQL migration for our empty testing DB
+  const { statementsToExecute } = await pushSQLiteSchema(schema, db as any)
 
-  // IDEA: Alternatively, take inspiration from https://github.com/drizzle-team/drizzle-orm/blob/main/drizzle-kit/src/cli/commands/push.ts
+  // TODO: The columns are using `camel_case` instead of `snake_case`
+  console.log(statementsToExecute.join('\n'))
 
-  // IDEA: However, a more future-proof approach could be to just generate the migration files and run them one statement at a time.
-  // One approach to running the statements: https://www.answeroverflow.com/m/1307948769480015883
+  // TODO: Verify if pushSQLiteSchema supports the `casing` option and try to set it to `snake_case`
+  // TODO: If that doesn't work, maybe we could manually create the JSON snapshots together with the migration files?
+  // I think I saw some of those methods supporting the `casing` option.
 
-  // TODO: empty the tmp migrations directory if it exists
-  // create tmp dir to create the drizzle-kit "initial" migration.
-  // read the tmp file
-  // exec migration
+  // Apply the migration to the temporary testing DB
+  db.$client.exec(statementsToExecute.join('\n'))
 
-  // IDEA: Maybe execute raw SQL
-  // db.$client.exec()
+  // Add common seeding data which is needed by the application
+  // IDEA: What if we add an admin user by default so test suites don't have to do that?
+  // However, then we use different
+  await addSeedingData(db)
 
-  mock.module('#db/index.ts', { cache: true, namedExports: { db } })
+  // TODO: verify the order statuses were seeded as expected. Remove this when done.
+  console.log(
+    'order statuses:',
+    db.select().from(schema.orderStatusTable).all(),
+  )
 
-  // import the seeding TS script and execute it against the in-memory db instance
+  // TODO: Is it possible to mock the #db/index.ts module directly in here?
+  // since it is imported by the same process as the node:test suite
+  // OR do we need to create the mock in the suite itself?
+  // mock.module('#db/index.ts', { cache: true, namedExports: { db } })
 }
+
+await setupMockedInMemoryTestDB()
