602f287259b783f169958af95ffb5991ab62ac8b
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-11-30 13:09:22 +0100

    Update note on future work to simplify tests by refactoring the backend to use dependency injection

--- backend/src/modules/auth/auth.routes.test.ts
+++ backend/src/modules/auth/auth.routes.test.ts
@@ -3,10 +3,12 @@ import { suite, test, type TestContext } from 'node:test'
 import { setupMockedInMemoryTestDB } from '#db/test-db.ts'
 import apiConfig from '#config/api.ts'
 
-// IDEA: If we execute on import, this could just be the first import and the rest would work as normal
-// This would remove the need for dynamic imports of the app. Just add the test db mock at the top of the test suite imports and you're good to go
-// A simpler API is much better, especially since we don't need to return the refercene to the `db` instance. We can just use it from the DB module as normal.
-// import '#db/setup-test-db.ts'
+// TODO: Simplify how the testing DB is set up by allowing it to be passed as a dependency to the app instance.
+// The app instance would then make the DB connection available for all services and controllers
+// which would remove the need for explicit imports of the DB module.
+// By using Dependency Injection (DI), we would make testing much easier too,
+// since we could just create a testing DB and pass it to the app instance.
+// We should evaluate options, and find a simple solution that helps us create a good structure for the backend.
 
 await setupMockedInMemoryTestDB()
 
