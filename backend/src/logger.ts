856d14dc31816695b348810a1c721ad49285847a
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-12-03 23:17:26 +0100

    Fix logger type error

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -1,6 +1,9 @@
+import type { FastifyBaseLogger } from 'fastify'
 import pino from 'pino'
 import apiConfig from '#config/api.ts'
 
 export function createLogger() {
-  return pino(apiConfig.logger)
+  // Override to fix type error due to ESM import
+  // More info: https://github.com/fastify/fastify/issues/5734
+  return pino(apiConfig.logger) as unknown as FastifyBaseLogger
 }
