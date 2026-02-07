a4dbf877650d3ec967d33ff0fc333076b04d3f5d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-02-07 19:20:56 +0100

    Use consistent classes

--- frontend/src/lib/components/section-cards.svelte
+++ frontend/src/lib/components/section-cards.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-  class="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card"
+  class="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card"
 >
   <Card.Root class="@container/card">
     <Card.Header>
