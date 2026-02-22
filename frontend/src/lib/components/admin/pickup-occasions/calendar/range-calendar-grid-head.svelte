e9e12c21d7743840fd963c367340fe6fdc05bd18
paccao <16489683+paccao@users.noreply.github.com>
2026-02-22 12:59:34 +0100

    add calendar related components

---
+++ frontend/src/lib/components/admin/pickup-occasions/calendar/range-calendar-grid-head.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { RangeCalendar as RangeCalendarPrimitive } from 'bits-ui'
+  let {
+    ref = $bindable(null),
+    ...restProps
+  }: RangeCalendarPrimitive.GridHeadProps = $props()
+</script>
+
+<RangeCalendarPrimitive.GridHead bind:ref {...restProps} />
