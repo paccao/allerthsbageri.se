6b887bb4612e371e9bbbb62f1eb3d36cfd0e312d
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2026-07-12 14:01:07 +0200

    clarify db:seed package.json to be dev only

--- backend/src/utils/dev-db-check.ts
+++ backend/src/utils/dev-db-check.ts
@@ -49,7 +49,7 @@ function ensureCorrectDatabaseState() {
   // or after resetting the dev DB.
   if (shouldAddSeedingData) {
     console.log('[dev]: No DB found. Creating a new one with seeding data.')
-    execSync('node --run db:seed')
+    execSync('node --run db:dev-seed')
   }
 }
 
